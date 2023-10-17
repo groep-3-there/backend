533f4d6cb1c427cdf3e9f8611e7816fdec841498
Florijn Munster <florijn.munster@student.nhlstenden.com>
2023-10-17 12:00:25 +0200

    Basic example
    Has a basic controller
    Has a basic entity model
    Has a basic repository

---
+++ src/main/java/matchmaker/backend/controllers/UserController.java
@@ -0,0 +1,22 @@
+package matchmaker.backend.controllers;
+
+import matchmaker.backend.models.User;
+import matchmaker.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public Iterable<User> getUsers() {
+       return userRepository.findAll();
+    }
+
+}
---
+++ src/main/java/matchmaker/backend/models/User.java
@@ -0,0 +1,40 @@
+package matchmaker.backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@EnableAutoConfiguration
+public class User {
+
+    @Id
+    @UuidGenerator
+    public UUID UUID;
+
+    public String name;
+    public String info;
+    public String tags;
+    public Date created_at;
+    public Date last_seen;
+    public Long avatar_image_id;
+    public boolean is_email_public;
+    public boolean is_phone_number_public;
+    public Date accepted_tos_date;
+    public Long role_id;
+    public Long department_id;
+    public Long company_id;
+
+    public User(String name){
+        this.name = name;
+    }
+    public User(){
+
+    }
+}
