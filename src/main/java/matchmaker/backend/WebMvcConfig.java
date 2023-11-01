d71de54fe27713a1f64609cc4c39ebc3b821066d
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-01 21:03:36 +0100

    add cors and fix auth interceptor

--- src/main/java/matchmaker/backend/WebMvcConfig.java
+++ src/main/java/matchmaker/backend/WebMvcConfig.java
@@ -2,6 +2,8 @@ package matchmaker.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,10 +13,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
     public AuthInterceptor AuthInterceptorWithBean(){
+
         return new AuthInterceptor();
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(AuthInterceptorWithBean());
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+
 }
--- src/main/java/matchmaker/backend/controllers/AuthController.java
+++ src/main/java/matchmaker/backend/controllers/AuthController.java
@@ -3,10 +3,7 @@ package matchmaker.backend.controllers;
 import matchmaker.backend.models.User;
 import matchmaker.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
