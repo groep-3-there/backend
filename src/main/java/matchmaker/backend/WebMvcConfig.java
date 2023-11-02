63f875b7cc73a8b14b503659c5bea9490be619a6
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-02 19:23:21 +0100

    Update WebMvcConfig.java

--- src/main/java/matchmaker/backend/WebMvcConfig.java
+++ src/main/java/matchmaker/backend/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
 
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
     }
 
 }
