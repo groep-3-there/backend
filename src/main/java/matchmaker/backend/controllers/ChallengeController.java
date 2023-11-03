70ecac3160ff7ec80980cfcfe2bb05b393f57171
Rik <66070072+Rikk010@users.noreply.github.com>
2023-11-03 14:38:53 +0100

    add author and company to challenge

--- src/main/java/matchmaker/backend/controllers/ChallengeController.java
+++ src/main/java/matchmaker/backend/controllers/ChallengeController.java
@@ -46,7 +46,7 @@ public class ChallengeController {
     }
 
     @PostMapping(path = "/challenge")
-    public Optional<Challenge> createChallenge(@RequestBody Challenge newChallenge) {
+    public Optional<Challenge> createChallenge(@RequestBody Challenge newChallenge, @RequestAttribute("loggedInUser") User currentUser) {
         Challenge checkedChallenge = new Challenge();
 
         //Only copy values we trust from the enduser. If user passes id, it is ignored.
@@ -61,6 +61,12 @@ public class ChallengeController {
         checkedChallenge.tags = newChallenge.tags;
         checkedChallenge.branch = newChallenge.branch;
         checkedChallenge.visibility = newChallenge.visibility;
+
+        //Set this based on the session, so no bad input can set the author, company & department
+        checkedChallenge.author = currentUser;
+        checkedChallenge.company = currentUser.company;
+        checkedChallenge.department = currentUser.department;
+        checkedChallenge.createdAt = new Date();
 
         Challenge savedChallenge = repository.save(checkedChallenge);
         return Optional.of(savedChallenge);
