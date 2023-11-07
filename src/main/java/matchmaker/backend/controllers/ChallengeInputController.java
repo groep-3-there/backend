593c9f1bf02e0045939df2040b382de72a2c06af
Florijn Munster <florijn.munster@student.nhlstenden.com>
2023-11-07 14:37:45 +0100

    Updates
    Wasn't working, changed the code to get the challenge by the id in the query

--- src/main/java/matchmaker/backend/controllers/ChallengeInputController.java
+++ src/main/java/matchmaker/backend/controllers/ChallengeInputController.java
@@ -1,8 +1,10 @@
 package matchmaker.backend.controllers;
 
 import matchmaker.backend.constants.ChallengeStatus;
+import matchmaker.backend.models.Challenge;
 import matchmaker.backend.models.ChallengeInput;
 import matchmaker.backend.models.User;
+import matchmaker.backend.repositories.ChallengeRepository;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import matchmaker.backend.repositories.ChallengeInputRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ChallengeInputController {
@@ -19,9 +22,12 @@ public class ChallengeInputController {
     @Autowired
     private ChallengeInputRepository repository;
 
+    @Autowired
+    private ChallengeRepository challengeRepository;
+
     @GetMapping("/reaction/challenge/{id}")
     public ResponseEntity<Iterable<ChallengeInput>> getChallengeReactions(@PathVariable("id") Long id) {
-        List<ChallengeInput> reactions = repository.findByChallengeId(id);
+        List<ChallengeInput> reactions = repository.findAllByChallengeId(id);
         return new ResponseEntity<>(reactions, HttpStatus.OK);
     }
 
@@ -38,20 +44,24 @@ public class ChallengeInputController {
     public ResponseEntity createReactionOnChallenge(@RequestBody ChallengeInput reaction,
                                                     @RequestAttribute("loggedInUser") User currentUser,
                                                     @PathVariable("id") Long challengeId){
-        //Check if the user is logged in
+        //Check if the user is logged inrea
         if(currentUser == null){ return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); }
 
         //Check if the challenge exists
-        if(reaction.challenge == null){ return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null); }
+        Optional<Challenge> challenge = challengeRepository.findById(challengeId);
+        if(challenge.isEmpty()){ return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); }
+
+        Challenge filledChallenge = challenge.get();
 
         //Check if the challenge is open for ideas
-        if(reaction.challenge.status != ChallengeStatus.OPEN_VOOR_IDEEEN){ return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null); }
+        if(filledChallenge.status != ChallengeStatus.OPEN_VOOR_IDEEEN){ return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null); }
 
         //If the user cant see the challenge, it should not be able to react to it.
-        if(!reaction.challenge.canBeSeenBy(currentUser)){return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);}
+        if(!filledChallenge.canBeSeenBy(currentUser)){return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);}
 
         //The user can see the challenge, and the challenge is open for ideas, so we can save the reaction.
         reaction.author = currentUser;
+        reaction.challenge = filledChallenge;
         repository.save(reaction);
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
--- src/main/java/matchmaker/backend/repositories/ChallengeInputRepository.java
+++ src/main/java/matchmaker/backend/repositories/ChallengeInputRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ChallengeInputRepository extends CrudRepository<ChallengeInput, Long> {
-    List<ChallengeInput> findByChallengeId(Long challengeId);
+    List<ChallengeInput> findAllByChallengeId(Long challengeId);
 }
