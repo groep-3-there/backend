6716004a649788e13a6dcf6e22f1225d64c19fb8
Rik <rikw2412@gmail.com>
2023-11-13 11:19:48 +0100

    fix origins

--- src/main/java/matchmaker/backend/WebMvcConfig.java
+++ src/main/java/matchmaker/backend/WebMvcConfig.java
@@ -24,10 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
 
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
-        registry.addMapping("/**").allowedOrigins("http://161.35.84.133:3000").allowedMethods("*");
-        registry.addMapping("/**").allowedOrigins("http://161.35.84.133").allowedMethods("*");
-        registry.addMapping("/**").allowedOrigins("https://groep-3-there.github.io").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://161.35.84.133:3000", "http://161.35.84.133","https://groep-3-there.github.io").allowedMethods("*");
 
 
     }
