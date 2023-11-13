5183c832eb1546979dd76454bb6ae17849fb6195
Rik <rikw2412@gmail.com>
2023-11-13 10:36:14 +0100

    whitelist ips

--- src/main/java/matchmaker/backend/WebMvcConfig.java
+++ src/main/java/matchmaker/backend/WebMvcConfig.java
@@ -25,6 +25,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
 
         registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://161.35.84.133:3000").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://161.35.84.133").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("https://groep-3-there.github.io").allowedMethods("*");
+
+
     }
 
 }
