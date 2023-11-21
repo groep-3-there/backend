02ffe010b848d74ededea3f431da53a892929d2c
TjerkVenema <Tjerk.Venema@student.nhlstenden.com>
2023-11-21 11:20:38 +0100

    Update UserController.java

--- src/main/java/matchmaker/backend/controllers/UserController.java
+++ src/main/java/matchmaker/backend/controllers/UserController.java
@@ -9,10 +9,7 @@ import matchmaker.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.regex.Pattern;
