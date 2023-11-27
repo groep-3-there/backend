daff396ba0d987c64e027aad61d98ed1572d1bc6
Florijnm <florijnmunster@outlook.com>
2023-11-27 11:25:10 +0100

    Update

--- src/test/java/matchmaker/backend/IntegrationTests/UserIntegrationTest.java
+++ src/test/java/matchmaker/backend/IntegrationTests/UserIntegrationTest.java
@@ -35,7 +35,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@Transactional
 public class UserIntegrationTest {
 
 
