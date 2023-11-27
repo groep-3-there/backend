48fdb462516b82969c3bf59b6379a5171f0995e7
Rik <rikw2412@gmail.com>
2023-11-27 11:44:19 +0100

    add missing perm

--- src/main/java/matchmaker/backend/constants/Perm.java
+++ src/main/java/matchmaker/backend/constants/Perm.java
@@ -7,5 +7,6 @@ public class Perm {
   public static final String DEPARTMENT_CREATE = "DEPARTMENT_CREATE";
   public static final String COMPANY_EDIT = "COMPANY_EDIT";
   public static final String COMPANY_GRADE = "COMPANY_GRADE";
+  public static final String COMPANY_MANAGE = "COMPANY_MANAGE";
   public static final String DEPARTMENT_MANAGE = "DEPARTMENT_MANAGE";
 }
