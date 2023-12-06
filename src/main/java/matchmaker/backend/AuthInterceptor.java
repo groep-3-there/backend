fbc407acc4998cc244f6132f9c132cd4e541e8e2
_Luke <70942550+LukevandePol@users.noreply.github.com>
2023-12-06 14:17:16 +0100

    Feature ds56 datapunten (#114)
    Added endpoints for Admin data
    Added test
    
    ---------
    
    Co-authored-by: github-actions <>
    Co-authored-by: Rik <rikw2412@gmail.com>

--- src/main/java/matchmaker/backend/AuthInterceptor.java
+++ src/main/java/matchmaker/backend/AuthInterceptor.java
@@ -6,14 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import matchmaker.backend.models.User;
 import matchmaker.backend.repositories.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Objects;
 import java.util.Optional;
 
 /** Adds the current logged in user to every request */
@@ -27,18 +26,15 @@ public class AuthInterceptor implements HandlerInterceptor {
   @Autowired private FirebaseApp firebaseApp;
 
   @Autowired private Environment environment;
-  private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
     if (request.getUserPrincipal() == null) {
-      log.info("[Auth Interceptor] No user principal found");
-      if (environment.getProperty("spring.profiles.active").equals("test")
+      if (Objects.equals(environment.getProperty("spring.profiles.active"), "test")
           && userRepository.existsById(1L)) {
         User testUser = userRepository.findById(1L).get();
         if (request.getParameter("loggedOut") != null) {
-          log.info("[Auth Interceptor] Forcing Logged out action");
           request.setAttribute("loggedInUser", null);
           return true;
         }
@@ -52,13 +48,11 @@ public class AuthInterceptor implements HandlerInterceptor {
     String firebaseUID = firebaseAuth.getUser(request.getUserPrincipal().getName()).getUid();
     Optional<User> loggedInUser = userRepository.findByFirebaseId(firebaseUID);
     if (loggedInUser.isEmpty()) {
-      log.info("[Auth Interceptor] No matching user found for firebase id " + firebaseUID);
       request.setAttribute("loggedInUser", null);
       return true;
     }
 
     User existingUser = loggedInUser.get();
-    log.info("[Auth Interceptor] Request performed by " + existingUser.name);
     request.setAttribute("loggedInUser", existingUser);
 
     return true; // Continue processing the request
