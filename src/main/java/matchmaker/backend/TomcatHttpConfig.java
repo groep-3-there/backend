35c61ab2fc9c911f7c75a6fafae1943e59efa2d2
Florijn Munster <florijn.munster@student.nhlstenden.com>
2023-12-14 10:07:43 +0100

    Added HTTP support in production

---
+++ src/main/java/matchmaker/backend/TomcatHttpConfig.java
@@ -0,0 +1,28 @@
+package matchmaker.backend;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpConfig {
+    //HTTP port
+    @Value("${http.port}")
+    private int httpPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
+}
